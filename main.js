3af8fe5b4da53e1c77e713ce7cd1e3179b06bd5b
Dylan  Bennett <dylanbennett@Dylans-MacBook-Air-2.local>
2026-01-28 12:34:29 +0000

    Add electron wrapper

---
+++ main.js
@@ -0,0 +1,27 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true
+    }
+  });
+  
+  // Clear cache and load homepage
+  win.webContents.session.clearCache();
+  win.loadURL('http://127.0.0.1:5000/');
+  win.webContents.openDevTools();
+  
+  win.webContents.on('did-finish-load', () => {
+    console.log('Loaded:', win.webContents.getURL());
+  });
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
